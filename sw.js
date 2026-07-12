b629010aa0c731d6843c66620d67c7f4a72601d6
Ajeyrana <147842149+Ajeyrana@users.noreply.github.com>
2026-07-12 22:06:10 +0530

    Add service worker for caching assets
    Implement service worker to cache assets for offline use.

---
+++ sw.js
@@ -0,0 +1,40 @@
+// Change this cache name number whenever you want to force users to clear cache and update!
+const CACHE_NAME = "till-pro-cache-v4";
+const ASSETS = [
+  "./",
+  "./index.html"
+];
+
+// Install Event: Save assets offline
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Activate Event: Clear out old cache versions
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch Event: Serve assets purely offline instantly
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
+
